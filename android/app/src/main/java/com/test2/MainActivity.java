36bf47dff05bda4f0cdeddd3d8b3c16324ebc2f1
Shrijan <shrijan.buzz.5@gmail.com>
2019-08-03 08:13:09 +0545

    working pushnotification in background

---
+++ android/app/src/main/java/com/test2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.test2;
+
+import com.facebook.react.ReactActivity;
+// import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;  // <--- Import Package
+
+public class MainActivity extends ReactActivity {
+
+    // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    //   @Override
+    //   protected boolean getUseDeveloperSupport() {
+    //     return BuildConfig.DEBUG;
+    //   }
+      
+    //   @Override
+    //   protected List<ReactPackage> getPackages() {
+    //         return Arrays.<ReactPackage>asList(
+    //             new MainReactPackage()
+    //             // new ReactNativePushNotificationPackage() // <---- Add the Package
+    //         );
+    //     }
+    // };
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "test2";
+    }
+}
