de544bd5f77c3c0c8e5bd2a285f8eacedf8f38ca
Shrijan <shrijan.buzz.5@gmail.com>
2019-08-03 08:50:46 +0545

    cleaned codes

--- android/app/src/main/java/com/test2/MainActivity.java
+++ android/app/src/main/java/com/test2/MainActivity.java
@@ -4,21 +4,6 @@ import com.facebook.react.ReactActivity;
 // import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;  // <--- Import Package
 
 public class MainActivity extends ReactActivity {
-
-    // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    //   @Override
-    //   protected boolean getUseDeveloperSupport() {
-    //     return BuildConfig.DEBUG;
-    //   }
-      
-    //   @Override
-    //   protected List<ReactPackage> getPackages() {
-    //         return Arrays.<ReactPackage>asList(
-    //             new MainReactPackage()
-    //             // new ReactNativePushNotificationPackage() // <---- Add the Package
-    //         );
-    //     }
-    // };
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
